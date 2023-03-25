ac7ef720fed0491575e949937689d66008f5958e
panteliselef <panteliselef@outlook.com>
2023-03-25 15:29:21 +0100

    Add vercel adapter

--- apps/playground/astro.config.mjs
+++ apps/playground/astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-import node from "@astrojs/node";
-import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: 'server',
+  adapter: vercel(),
 });
