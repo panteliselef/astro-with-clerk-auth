fc2b827b80b979fefbc39fadb89077bd69e52300
panteliselef <panteliselef@outlook.com>
2023-04-16 01:15:46 +0300

    Add vercel edge middleware

---
+++ packages/astro-clerk-auth/src/middleware/utils.ts
@@ -0,0 +1,151 @@
+import { constants } from "@clerk/backend";
+
+import type { RequestLike } from "./types";
+
+type AuthKey = "AuthStatus" | "AuthMessage" | "AuthReason";
+
+export function setCustomAttributeOnRequest(
+  req: RequestLike,
+  key: string,
+  value: string
+): void {
+  Object.assign(req, { [key]: value });
+}
+
+export function getCustomAttributeFromRequest(
+  req: RequestLike,
+  key: string
+): string | null | undefined {
+  // @ts-expect-error
+  return key in req ? req[key] : undefined;
+}
+
+export function getAuthKeyFromRequest(
+  req: RequestLike,
+  key: AuthKey
+): string | null | undefined {
+  return (
+    getCustomAttributeFromRequest(req, constants.Attributes[key]) ||
+    getHeader(req, constants.Headers[key]) ||
+    (key === "AuthStatus"
+      ? getQueryParam(req, constants.SearchParams.AuthStatus)
+      : undefined)
+  );
+}
+
+// Tries to extract auth status from the request using several strategies
+// TODO: Rename Auth status and align the naming across media
+export function getAuthStatusFromRequest(
+  req: RequestLike
+): string | null | undefined {
+  return (
+    getCustomAttributeFromRequest(req, constants.Attributes.AuthStatus) ||
+    getHeader(req, constants.Headers.AuthStatus) ||
+    getQueryParam(req, constants.SearchParams.AuthStatus)
+  );
+}
+
+function getQueryParam(
+  req: RequestLike,
+  name: string
+): string | null | undefined {
+  // Check if the request contains a parsed query object
+  // NextApiRequest does, but the IncomingMessage in the GetServerSidePropsContext case does not
+  let queryParam: string | null | undefined;
+  if ("query" in req) {
+    //@ts-expect-error
+    queryParam = req.query[name] as string | undefined;
+  }
+
+  // Fall back to query string
+  if (!queryParam) {
+    const qs = (req.url || "").split("?")[1];
+    queryParam = new URLSearchParams(qs).get(name);
+  }
+  return queryParam;
+}
+
+export function getHeader(
+  req: RequestLike,
+  name: string
+): string | null | undefined {
+  // If no header has been determined for IncomingMessage case, check if available within private `socket` headers
+  // When deployed to vercel, req.headers for API routes is a `IncomingHttpHeaders` key-val object which does not follow
+  // the Headers spec so the name is no longer case-insensitive.
+  // @ts-expect-error
+  return req.headers[name] || req.headers[name.toLowerCase()];
+}
+
+export const parseCookie = (str: string) => {
+  if (!str) return {};
+  return str
+    .split(";")
+    .map((v) => v.split("="))
+    .reduce(
+      (
+        acc: {
+          [key: string]: string;
+        },
+        v
+      ) => {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+        return acc;
+      },
+      {}
+    );
+};
+
+export function getCookie(req: RequestLike, name: string): string | undefined {
+  return parseCookie(req.headers.get("cookie") ?? "")[name];
+}
+
+export const injectSSRStateIntoObject = <O, T>(obj: O, authObject: T) => {
+  // Serializing the state on dev env is a temp workaround for the following issue:
+  // https://github.com/vercel/next.js/discussions/11209|Next.js
+  const __clerk_ssr_state = (
+    process.env.NODE_ENV !== "production"
+      ? JSON.parse(JSON.stringify({ ...authObject }))
+      : { ...authObject }
+  ) as T;
+  return { ...obj, __clerk_ssr_state };
+};
+
+// TODO: Use the same function defined in @clerk/shared once the package is tree shakeable
+type VOrFnReturnsV<T> = T | undefined | ((v: URL) => T);
+export function handleValueOrFn<T>(
+  value: VOrFnReturnsV<T>,
+  url: URL
+): T | undefined;
+export function handleValueOrFn<T>(
+  value: VOrFnReturnsV<T>,
+  url: URL,
+  defaultValue: T
+): T;
+export function handleValueOrFn<T>(
+  value: VOrFnReturnsV<T>,
+  url: URL,
+  defaultValue?: unknown
+): unknown {
+  if (typeof value === "function") {
+    return (value as (v: URL) => T)(url);
+  }
+
+  if (typeof value !== "undefined") {
+    return value;
+  }
+
+  if (typeof defaultValue !== "undefined") {
+    return defaultValue;
+  }
+
+  return undefined;
+}
+
+// TODO: use @clerk/shared once it is tree-shakeable
+export function isHttpOrHttps(key: string | undefined) {
+  return /^http(s)?:\/\//.test(key || "");
+}
+
+export function isDevelopmentFromApiKey(apiKey: string): boolean {
+  return apiKey.startsWith("test_") || apiKey.startsWith("sk_test_");
+}
