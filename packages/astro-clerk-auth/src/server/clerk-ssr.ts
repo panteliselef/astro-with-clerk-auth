1289d43035519f82539ca933fc29b57ba7adfd2c
panteliselef <panteliselef@outlook.com>
2023-12-20 23:49:47 +0200

    feat: Expose react component as Astro components (#17)

--- packages/astro-clerk-auth/src/server/clerk-ssr.ts
+++ packages/astro-clerk-auth/src/server/clerk-ssr.ts
@@ -3,5 +3,6 @@ import { AstroGlobal } from 'astro';
 import { $ssrState } from '../stores/internal';
 
 export const clerkSSR = (astro: AstroGlobal) => {
+  // @ts-ignore TODO: fix this
   $ssrState.set(astro.locals.auth());
 };
