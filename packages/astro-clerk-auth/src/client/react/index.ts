37b72158a3359ab64bcd9d7f6b8f500b1df7aeae
panteliselef <panteliselef@outlook.com>
2023-12-27 00:09:33 +0200

    feat: Introduce `updateClerkOptions`

--- packages/astro-clerk-auth/src/client/react/index.ts
+++ packages/astro-clerk-auth/src/client/react/index.ts
@@ -6,15 +6,18 @@ import { AstroClerkIntegrationParams } from '../../types';
 export * from './uiComponents';
 export * from './controlComponents';
 
+let initOptions: AstroClerkIntegrationParams | undefined;
+
 export function createClerkInstance(options?: AstroClerkIntegrationParams) {
-  let clerkJSInstance = window.Clerk as Clerk
+  let clerkJSInstance = window.Clerk as Clerk;
   if (!clerkJSInstance) {
-    clerkJSInstance = new Clerk(publishableKey)
+    clerkJSInstance = new Clerk(publishableKey);
     $clerk.set(clerkJSInstance);
     window.Clerk = clerkJSInstance;
   }
   // window.$derivedState = $derivedState;
 
+  initOptions = options;
   return clerkJSInstance
     .load(options)
     .then(() => {
@@ -28,4 +31,15 @@ export function createClerkInstance(options?: AstroClerkIntegrationParams) {
       });
     })
     .catch(() => {});
+}
+
+export function updateClerkOptions(options: AstroClerkIntegrationParams) {
+  const clerk = $clerk.get();
+  if (!clerk) {
+    throw new Error('Missing clerk instance');
+  }
+  clerk.__unstable__updateProps({
+    options: {...initOptions, ...options},
+    appearance: { ...initOptions?.appearance, ...options.appearance },
+  });
 }
