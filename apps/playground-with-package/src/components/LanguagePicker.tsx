8baefd38965b979761fe62b7ac2895772a9a9ac2
panteliselef <panteliselef@outlook.com>
2023-12-27 00:26:07 +0200

    feat: Introduce updateClerkOptions (#19)
    * feat: Make integration configurable
    
    * feat: Introduce `updateClerkOptions`
    
    * feat: Introduce `updateClerkOptions`

---
+++ apps/playground-with-package/src/components/LanguagePicker.tsx
@@ -0,0 +1,28 @@
+import { updateClerkOptions } from "astro-clerk-auth/client/react";
+import React, { type ChangeEvent } from "react";
+
+export function LanguagePicker() {
+  const onChange = async (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    let localization: any;
+
+    if (value === "fr") {
+      localization = (await import("@clerk/localizations")).frFR;
+    } else {
+      localization = undefined;
+    }
+
+    updateClerkOptions({
+      localization,
+    });
+  };
+  return (
+    <select
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      onChange={onChange}
+    >
+      <option value="en">English</option>
+      <option value="fr">French</option>
+    </select>
+  );
+}
