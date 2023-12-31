b6a797001ea3c1e24cadc43976a0314177ac021e
panteliselef <panteliselef@outlook.com>
2023-12-31 19:00:03 +0200

    fix: Fix missing config for bundle visualizer

--- apps/playground-with-package/astro.config.mjs
+++ apps/playground-with-package/astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import astroClerk from "astro-clerk-auth/integration";
-// import node from '@astrojs/node';
+// import node from "@astrojs/node";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +18,30 @@ export default defineConfig({
       afterSignUpUrl: "/",
     }),
   ],
+  vite: {
+    plugins: [
+      visualizer({
+        template: "treemap",
+        open: process.env.ANALYZE==='true',
+        gzipSize: true,
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("clerk-js")) {
+              return "@clerk-js";
+            }
+
+            if (id.includes("localizations")) {
+              return "@clerk-localizations";
+            }
+          },
+        },
+      },
+    },
+  },
   output: "server",
   adapter: vercel({
     webAnalytics: {
