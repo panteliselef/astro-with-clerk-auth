773d4295871648b729a0c45dca153c297b1b33ac
panteliselef <panteliselef@outlook.com>
2024-01-04 01:45:21 +0200

    feat: Replace `astro-components` path with `components` (#27)
    * feat: Replace `astro-components` path with `components`
    
    Created 'components/control' and 'components/react'
    
    * fix: Update Layout to use new import path
    
    * fix: missing files

--- packages/astro-clerk-auth/src/astro-components/react/index.ts
+++ packages/astro-clerk-auth/src/astro-components/react/index.ts
@@ -1,8 +1,6 @@
 // The `ts-ignore` comments here are necessary because we're importing this file inside the `astro:components`
 // virtual module's types, which means that `tsc` will try to resolve these imports. Don't mind the editor errors.
 // @ts-ignore
-export { default as ClerkLayout } from './ClerkLayout.astro';
-// @ts-ignore
 export { default as SignIn } from './SignIn.astro';
 // @ts-ignore
 export { default as SignUp } from './SignUp.astro';
