ac95a96bccb375e205723285e9096cf55bfb1630
panteliselef <panteliselef@outlook.com>
2024-01-06 01:35:28 +0200

    feat: Clerk UI Components without react (#29)

--- packages/astro-clerk-auth/src/types.ts
+++ packages/astro-clerk-auth/src/types.ts
@@ -1,6 +1,15 @@
-import { ClerkOptions, MultiDomainAndOrProxyPrimitives, Without } from '@clerk/types';
+import type { Clerk } from '@clerk/clerk-js';
+import type { ClerkOptions, MultiDomainAndOrProxyPrimitives, Without } from '@clerk/types';
 export type AstroClerkIntegrationParams = Without<
   ClerkOptions,
   'isSatellite' | 'sdkMetadata' | 'telemetry' | 'standardBrowser' | 'selectInitialSession'
 > &
   MultiDomainAndOrProxyPrimitives;
+
+declare global {
+  interface Window {
+    componentPropsMap?: Map<'user-button', Map<string, Record<string, unknown>>>;
+  }
+}
+
+export {};
