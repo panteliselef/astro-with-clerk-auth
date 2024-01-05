857a5ded777a529c8ebde457eb8e9de999f85e9f
panteliselef <panteliselef@outlook.com>
2024-01-05 17:07:47 +0200

    feat: SignedIn astro server components

--- packages/astro-clerk-auth/src/astro-components/control/index.ts
+++ packages/astro-clerk-auth/src/astro-components/control/index.ts
@@ -2,4 +2,8 @@
 // virtual module's types, which means that `tsc` will try to resolve these imports. Don't mind the editor errors.
 // @ts-ignore
 export { default as ClerkLayout } from './ClerkLayout.astro';
+// @ts-ignore
+export { default as SignedIn } from './SignedIn.astro';
+// @ts-ignore
+export { default as SignedOut } from './SignedOut.astro';
 
