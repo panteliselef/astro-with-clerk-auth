2d2a10b734172e24d3084790d7543f88701af71e
panteliselef <panteliselef@outlook.com>
2024-01-13 16:45:01 +0200

    fix: Solved Auth.locals.auth not being a function (#36)

--- packages/astro-clerk-auth/src/server/types.ts
+++ packages/astro-clerk-auth/src/server/types.ts
@@ -3,4 +3,6 @@ import type { defineMiddleware } from 'astro/middleware';
 export type AstroMiddleware = Parameters<typeof defineMiddleware>[0];
 export type AstroMiddlewareContextParam = Parameters<AstroMiddleware>['0'];
 export type AstroMiddlewareNextParam = Parameters<AstroMiddleware>['1'];
-export type AstroMiddlewareReturn = Promise<void> | void | Response | Promise<Response>;
+export type AstroMiddlewareReturn =
+  // Promise<void> | void |
+  Response | Promise<Response>;
