704b97b137f4a997669f3699d455a65530df87c1
panteliselef <panteliselef@outlook.com>
2024-01-28 15:05:20 +0200

    chore: Fix 500 on index

--- apps/playground/astro.config.mjs
+++ apps/playground/astro.config.mjs
@@ -1,28 +1,32 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import astroClerk from "astro-clerk-auth";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind(),
-        astroClerk({
-            signInUrl:'/sign-in',
-            signUpUrl:'/sign-up'
-        })
-    ],
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-        speedInsights: {
-            enabled: true,
-        },
+  integrations: [
+    react(),
+    tailwind(),
+    astroClerk({
+      signInUrl: "/sign-in",
+      signUpUrl: "/sign-up",
     }),
+  ],
+  output: "server",
+  adapter: vercel({
+      webAnalytics: {
+          enabled: true,
+      },
+      speedInsights: {
+          enabled: true,
+      },
+  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
