5009277bf85fa785bfebad50aac8f1bf5d9b47e6
panteliselef <panteliselef@outlook.com>
2024-01-28 14:53:56 +0200

    chore: Use latest astro-clerk-auth for personal website (#57)

--- apps/playground/src/pages/api/guestbook.ts
+++ apps/playground/src/pages/api/guestbook.ts
@@ -1,65 +1,61 @@
-import type { APIRoute } from 'astro';
-import { getAuth, clerkClient } from 'astro-clerk-auth/v0';
-import { queryBuilder } from '../../db/planetscale';
-import { getGuestbook } from '../../utils';
+import type { APIRoute } from "astro";
+import { clerkClient } from "astro-clerk-auth/v0";
+import { queryBuilder } from "../../db/planetscale";
+import { getGuestbook } from "../../utils";
 
-export const post: APIRoute = async ({ request }) => {
-  if (request.headers.get('Content-Type') === 'application/json') {
-    const body = await request.json();
-    const { message } = body;
-    try {
-      const auth = await getAuth({ server: request });
-
-      if (auth instanceof Response) {
-        throw new Error('not logged in');
-      }
-
-      if (!auth.userId) {
-        throw new Error('not logged in');
-      }
-
-      const { data } = await clerkClient.users.getUser(auth.userId);
-
-      if (!data) throw new Error('Email not found');
-
-      const { primaryEmailAddressId, username } = data;
-
-      if (!primaryEmailAddressId) throw new Error('Email not found');
-      if (!username) throw new Error('username not found');
-
-      await queryBuilder
-        .insertInto('guestbook')
-        .values({
-          email: primaryEmailAddressId,
-          message,
-          created_by: username,
-        })
-        .executeTakeFirst();
-
-      return new Response(
-        JSON.stringify({
-          error: null,
-        }),
-        {
-          status: 200,
-        },
-      );
-    } catch (e) {
-      console.log(e);
-      return new Response(
-        JSON.stringify({
-          message: 'Something went wrong',
-        }),
-        { status: 400 },
-      );
-    }
+export const POST: APIRoute = async ({ request, locals }) => {
+  if (request.headers.get("Content-Type") !== "application/json") {
+    return new Response(
+      JSON.stringify({
+        message: "Method not allowed",
+      }),
+      { status: 405 },
+    );
   }
-  return new Response(
-    JSON.stringify({
-      message: 'Method not allowed',
-    }),
-    { status: 405 },
-  );
+
+  const body = await request.json();
+  const { message } = body;
+  try {
+    const auth = locals.auth();
+
+    if (!auth.userId) {
+      throw new Error("not logged in");
+    }
+
+    const { data } = await clerkClient.users.getUser(auth.userId);
+
+    if (!data) throw new Error("Email not found");
+
+    const { primaryEmailAddressId, username } = data;
+
+    if (!primaryEmailAddressId) throw new Error("Email not found");
+    if (!username) throw new Error("username not found");
+
+    await queryBuilder
+      .insertInto("guestbook")
+      .values({
+        email: primaryEmailAddressId,
+        message,
+        created_by: username,
+      })
+      .executeTakeFirst();
+
+    return new Response(
+      JSON.stringify({
+        error: null,
+      }),
+      {
+        status: 200,
+      },
+    );
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        message: "Something went wrong",
+      }),
+      { status: 500 },
+    );
+  }
 };
 
 export const get: APIRoute = async () => {
@@ -70,12 +66,11 @@ export const get: APIRoute = async () => {
       status: 200,
     });
   } catch (e) {
-    console.log(e);
     return new Response(
       JSON.stringify({
-        message: 'Something went wrong',
+        message: "Something went wrong",
       }),
-      { status: 400 },
+      { status: 500 },
     );
   }
 };
