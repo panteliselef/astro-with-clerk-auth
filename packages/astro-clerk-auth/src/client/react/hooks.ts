69462c6177e98bb2474d8ed29b341cf76bdeb0dd
panteliselef <panteliselef@outlook.com>
2024-02-10 00:30:01 +0200

    chore: Add comments

--- packages/astro-clerk-auth/src/client/react/hooks.ts
+++ packages/astro-clerk-auth/src/client/react/hooks.ts
@@ -95,6 +95,15 @@ type UseAuth = () => UseAuthReturn;
 // TODO: Use the hook from nano stores
 export function useStore(store: Store, opts = {}) {
   return useSyncExternalStore(store.subscribe, store.get, () => {
+    // Per react docs
+    /**
+     * optional getServerSnapshot:
+     * A function that returns the initial snapshot of the data in the store.
+     * It will be used only during server rendering and during hydration of server-rendered content on the client.
+     * The server snapshot must be the same between the client and the server, and is usually serialized and passed from the server to the client.
+     * If you omit this argument, rendering the component on the server will throw an error.
+     */
+
     if (typeof window === 'undefined') {
       return authAsyncStorage.getStore();
     }
