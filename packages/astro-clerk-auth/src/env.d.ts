e400bcc744bebf80da7635ac970b41186277deb4
panteliselef <panteliselef@outlook.com>
2024-04-09 22:21:28 +0300

    chore: Remove unused env var types

--- packages/astro-clerk-auth/src/env.d.ts
+++ packages/astro-clerk-auth/src/env.d.ts
@@ -11,8 +11,6 @@ interface ImportMetaEnv {
   readonly CLERK_API_VERSION?: string;
   readonly CLERK_JWT_KEY?: string;
   readonly CLERK_SECRET_KEY?: string;
-  readonly DATABASE_URL?: string;
-  readonly FLAG_GUESTBOOK?: string;
 }
 
 interface ImportMeta {
