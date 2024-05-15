1d8ec4015d6410c30387a9690882a2c13b08381d
panteliselef <panteliselef@outlook.com>
2024-05-16 00:58:35 +0300

    Revert "fix: Avoid importing async_hooks in the client (#133)"
    This reverts commit 85b87a5d

--- packages/astro-clerk-auth/tsup.config.ts
+++ packages/astro-clerk-auth/tsup.config.ts
@@ -27,6 +27,6 @@ export default defineConfig(() => {
     bundle: true,
     sourcemap: true,
     format: ['esm'],
-    external: ['astro', 'react', 'react-dom', 'clerk:astro', 'virtual:astro-clerk'],
+    external: ['astro', 'react', 'react-dom', 'node:async_hooks'],
   };
 });
