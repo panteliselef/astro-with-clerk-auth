216f5408cc01d9402c2e76927a00933da5fab006
panteliselef <panteliselef@outlook.com>
2024-05-17 11:33:15 +0300

    Revert "Revert "fix: Avoid importing async_hooks in the client (#133)" (#142)"
    This reverts commit d87c55af9888d6a255ac26d3f38693cb7bad533d.

--- packages/astro-clerk-auth/src/internal/create-injection-script-runner.ts
+++ packages/astro-clerk-auth/src/internal/create-injection-script-runner.ts
@@ -2,13 +2,13 @@ import { CreateClerkInstanceInternalFn } from '../client/types';
 import { $initialState } from '../stores/internal';
 import { AstroClerkIntegrationParams } from '../types';
 import { mergeEnvVarsWithParams } from './merge-env-vars-with-params';
+import { getClerkAuthInitState } from 'clerk:astro';
 
 function createInjectionScriptRunner(creator: CreateClerkInstanceInternalFn) {
   async function runner(astroClerkOptions?: AstroClerkIntegrationParams) {
-    const ssrDataContainer = document.getElementById('__CLERK_ASTRO_DATA__');
-    if (ssrDataContainer) {
-      $initialState.set(JSON.parse(ssrDataContainer.textContent || '{}'));
-    }
+    // Sync SSR initState to the store
+    // @ts-ignore missing user, organization, session objects but this is expected
+    $initialState.set(getClerkAuthInitState());
 
     await creator(mergeEnvVarsWithParams(astroClerkOptions));
   }
